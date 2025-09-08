988145b28df09fcae848ad9e0414e40ed7aa3c3b
Tharusha-bit <tharushanimnathme@gmail.com>
2025-09-08 21:00:54 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -54,5 +54,10 @@ app.use("/api/messages", messageRouter);
 
 await connectDB();
 
-const port = process.env.PORT || 5000;
-server.listen(port, () => console.log("Server is running on port :" + port));
+if (process.env.NODE_ENV !== "production") {
+  const port = process.env.PORT || 5000;
+  server.listen(port, () => console.log("Server is running on port :" + port));
+}
+
+//Export server for vercel
+export default server;
